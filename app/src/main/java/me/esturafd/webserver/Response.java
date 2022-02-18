16bd7cec84080a1bf0c338278d0315b661fcad77
Estuardo Ramos <estuardo.ramos@esturafd.me>
2022-02-17 20:14:08 -0600

    refactor: change response implementation to template

--- app/src/main/java/me/esturafd/webserver/Response.java
+++ app/src/main/java/me/esturafd/webserver/Response.java
@@ -1,29 +1,34 @@
 package me.esturafd.webserver;
 
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 public class Response {
 
-    private List<String> content;
+    private String content;
+    private static final String RESPONSE_TEMPLATE = 
+        "HTTP/1.0 %d OK \n" +
+        "Date: %s \n" +
+        "Server: MiniWebServer(esturafd)/1.0 \n" +
+        "Content-Type: %s \n" +
+        "Expires: Thu 01 Dec 1994 16:00:00 GMT \n" +
+        "Content-Length: %d \n" +
+        "Last-Modified: %s \n\n%s";
 
     public Response(String message, String contentType, int code) {
-        content = Arrays.asList(
-            String.format("HTTP/1.0 %d OK \n", code),
-            String.format("Date: %s \n", new Date().toString()),
-            "Server: esturafd-server/1.0 \n",
-            String.format("Conten-Type: %s \n", contentType),
-            "Expires: Thu 01 Dec 1994 16:00:00 GMT \n",
-            String.format("Content-Length: %d \n", message.length()),
-            String.format("Last-modifed: %s \n\n", new Date(System.currentTimeMillis()).toString()),
+        content = String.format(
+            RESPONSE_TEMPLATE, 
+            code,
+            new Date().toString(),
+            contentType,
+            message.length(),
+            new Date(System.currentTimeMillis()).toString(),
             message
         );
     }
 
     @Override
     public String toString() {
-        return String.join("", content);
+        return content;
     }
 
     public byte[] getBytes() {
